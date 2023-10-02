5d816bc03f52390e9ea0aef3fcf317a4453bdad8
mse99 <mohamedsalemedrah@gmail.com>
2023-10-02 14:49:09 +0200

    Update liveview to 0.20 and remove live dashboard

--- lib/lukas_web/router.ex
+++ lib/lukas_web/router.ex
@@ -98,28 +98,6 @@ defmodule LukasWeb.Router do
     end
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", LukasWeb do
-  #   pipe_through :api
-  # end
-
-  # Enable LiveDashboard and Swoosh mailbox preview in development
-  if Application.compile_env(:lukas, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
-    import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through(:browser)
-
-      live_dashboard("/dashboard", metrics: LukasWeb.Telemetry)
-      forward("/mailbox", Plug.Swoosh.MailboxPreview)
-    end
-  end
-
   ## Authentication routes
 
   scope "/", LukasWeb do
