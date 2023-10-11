e7698f6e53d32179172fa01a00b58d84ca3bff3a
Mohamed Edrah <mohamedsalemedrah@gmail.com>
2023-10-11 10:10:00 +0200

    Asynchronously load student data

--- lib/lukas_web/live/operators/operator_student_live.ex
+++ lib/lukas_web/live/operators/operator_student_live.ex
@@ -3,26 +3,55 @@ defmodule LukasWeb.Operator.StudentLive do
 
   alias Lukas.Accounts
   alias Lukas.Learning.Course
+  alias Phoenix.LiveView.AsyncResult
 
   def mount(%{"id" => raw_student_id}, _, socket) do
     with {id, _} <- Integer.parse(raw_student_id),
-         student when student != nil <- Accounts.get_student(id),
-         courses <- Course.Students.list_student_courses(student) do
-      {:ok, socket |> assign(:student, student) |> stream(:courses, courses)}
+         student when student != nil <- Accounts.get_student(id) do
+      next_socket =
+        socket
+        |> stream_configure(:courses, [])
+        |> assign(:student, student)
+        |> assign(:loading, AsyncResult.loading())
+        |> start_async(:loading, fn -> Course.Students.list_student_courses(student) end)
+
+      {:ok, next_socket}
     else
       _ -> {:ok, redirect(socket, to: ~p"/controls/students")}
     end
+  end
+
+  def handle_async(:loading, {:ok, courses}, socket) do
+    next_socket =
+      socket
+      |> assign(:loading, AsyncResult.ok(socket.assigns.loading, nil))
+      |> stream(:courses, courses)
+
+    {:noreply, next_socket}
+  end
+
+  def handle_async(:loading, {:exit, reason}, socket) do
+    next_socket =
+      socket
+      |> assign(:loading, AsyncResult.failed(socket.assigns.loading, reason))
+
+    {:noreply, next_socket}
   end
 
   def render(assigns) do
     ~H"""
     <h1><%= @student.name %></h1>
 
-    <ul id="courses" phx-update="stream">
-      <li :for={{id, course} <- @streams.courses} id={id}>
-        <%= course.name %>
-      </li>
-    </ul>
+    <.async_result assign={@loading}>
+      <:loading>Loading...</:loading>
+      <:failed>Failed...</:failed>
+
+      <ul id="courses" phx-update="stream">
+        <li :for={{id, course} <- @streams.courses} id={id}>
+          <%= course.name %>
+        </li>
+      </ul>
+    </.async_result>
     """
   end
 end
--- test/lukas_web/live/operators/operator_student_live_test.exs
+++ test/lukas_web/live/operators/operator_student_live_test.exs
@@ -8,6 +8,10 @@ defmodule LukasWeb.Operators.StudentLive do
 
   setup :register_and_log_in_user
 
+  setup do
+    %{student: student_fixture()}
+  end
+
   test "should redirect if the user id is invalid.", %{conn: conn} do
     assert {:error, {:redirect, _}} = live(conn, ~p"/controls/students/INVALID")
   end
@@ -16,14 +20,15 @@ defmodule LukasWeb.Operators.StudentLive do
     assert {:error, {:redirect, _}} = live(conn, ~p"/controls/students/4050123")
   end
 
-  test "should render the student data.", %{conn: conn} do
+  test "should render the student data.", %{conn: conn, student: student} do
     course = course_fixture()
-    student = student_fixture()
 
     direct_deposit_fixture(user_fixture(), student, course.price)
     Lukas.Learning.Course.Students.enroll_student(course, student)
 
-    {:ok, _, html} = live(conn, ~p"/controls/students/#{student.id}")
+    {:ok, lv, _html} = live(conn, ~p"/controls/students/#{student.id}")
+
+    html = render_async(lv)
 
     assert html =~ student.name
     assert html =~ course.name
