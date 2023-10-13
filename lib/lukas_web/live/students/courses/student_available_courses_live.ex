594f88a5faf723a61f61e6523c1535ea8e28c28f
mse99 <mohamedsalemedrah@gmail.com>
2023-10-13 20:58:04 +0200

    Style student owned courses and available courses page
    closes #43
    closes #42

--- lib/lukas_web/live/students/courses/student_available_courses_live.ex
+++ lib/lukas_web/live/students/courses/student_available_courses_live.ex
@@ -14,11 +14,39 @@ defmodule LukasWeb.Students.AvailableCoursesLive do
 
   def render(assigns) do
     ~H"""
-    <h1>Available courses</h1>
+    <ul class="flex gap-1 text-lg text-secondary my-8">
+      <li>
+        <.link navigate={~p"/home"}>home</.link>
+      </li>
+      <li>
+        /
+      </li>
+      <li>
+        <.link navigate={~p"/home/courses/available"}>available courses</.link>
+      </li>
+    </ul>
 
     <ul id="available-courses" phx-update="stream">
-      <li :for={{id, course} <- @streams.available_courses} id={id}>
-        <.link navigate={~p"/home/courses/#{course.id}"}><%= course.name %></.link>
+      <li :for={{id, course} <- @streams.available_courses} id={id} class="mb-3 max-w-lg mx-auto">
+        <.link
+          navigate={~p"/home/courses/#{course.id}"}
+          class=" flex h-[104px] bg-white shadow rounded text-secondary"
+        >
+          <img
+            src={~p"/images/#{course.banner_image}"}
+            width={110}
+            height={104}
+            class="w-[110px] h-[104px] rounded-tl-lg rounded-bl-lg"
+          />
+
+          <div class="p-3">
+            <strong><%= course.name %></strong>
+
+            <p>
+              The description of the course lays here
+            </p>
+          </div>
+        </.link>
       </li>
     </ul>
     """
--- lib/lukas_web/live/students/courses/student_courses_live.ex
+++ lib/lukas_web/live/students/courses/student_courses_live.ex
@@ -11,11 +11,39 @@ defmodule LukasWeb.Students.CoursesLive do
 
   def render(assigns) do
     ~H"""
-    <h1>Courses</h1>
+    <ul class="flex gap-1 text-lg text-secondary my-8">
+      <li>
+        <.link navigate={~p"/home"}>home</.link>
+      </li>
+      <li>
+        /
+      </li>
+      <li>
+        <.link navigate={~p"/home/courses"}>my courses</.link>
+      </li>
+    </ul>
 
     <ul id="courses" phx-update="stream">
-      <li :for={{id, course} <- @streams.courses} id={id}>
-        <%= course.name %>
+      <li :for={{id, course} <- @streams.courses} id={id} class="mb-3 max-w-lg mx-auto">
+        <.link
+          navigate={~p"/home/courses/#{course.id}"}
+          class=" flex h-[104px] bg-white shadow rounded text-secondary"
+        >
+          <img
+            src={~p"/images/#{course.banner_image}"}
+            width={110}
+            height={104}
+            class="w-[110px] h-[104px] rounded-tl-lg rounded-bl-lg"
+          />
+
+          <div class="p-3">
+            <strong><%= course.name %></strong>
+
+            <p>
+              The description of the course lays here
+            </p>
+          </div>
+        </.link>
       </li>
     </ul>
     """
