fb714d5d934a6e55e82e16c1facd263f09fe0370
mse99 <mohamedsalemedrah@gmail.com>
2023-10-24 18:39:39 +0200

    Port operator lessons page and use it as lecturer page

---
+++ lib/lukas_web/live/lecturers/courses/lecturer_course_lessons_live.ex
@@ -0,0 +1,209 @@
+defmodule LukasWeb.Lecturer.CourseLessonsLive do
+  use LukasWeb, :live_view
+
+  alias Lukas.Learning
+  alias Lukas.Learning.Course.Staff
+
+  alias LukasWeb.CommonComponents
+  alias Phoenix.LiveView.AsyncResult
+
+  def mount(params, _session, socket) do
+    {:ok,
+     socket
+     |> stream_configure(:lessons, [])
+     |> assign(:loading, AsyncResult.loading())
+     |> start_async(:loading, fn ->
+       load_course_with_lessons(params, socket.assigns.current_user)
+     end)
+     |> assign(:show_form_modal, false)}
+  end
+
+  defp load_course_with_lessons(%{"id" => raw_id}, current_user) do
+    with {id, _} <- Integer.parse(raw_id),
+         course when course != nil <- Learning.get_course(id),
+         lecturers <- Staff.list_course_lecturers(course),
+         lect when lect != nil <- find_lecturer_in(lecturers, current_user) do
+      lessons = Learning.get_lessons(course)
+      {course, lessons}
+    else
+      _ -> :error
+    end
+  end
+
+  defp find_lecturer_in(lecturers, lect) do
+    Enum.find(lecturers, fn other -> other.id == lect.id end)
+  end
+
+  def handle_async(:loading, {:ok, {course, lessons}}, socket) do
+    Learning.watch_course(course)
+
+    form = Learning.Course.Content.create_lesson_changeset(course) |> to_form()
+
+    next_socket =
+      socket
+      |> assign(:loading, AsyncResult.ok(socket.assigns.loading, nil))
+      |> assign(:course, course)
+      |> assign(:form, form)
+      |> assign(:lesson, nil)
+      |> stream(:lessons, lessons)
+
+    {:noreply, next_socket}
+  end
+
+  def handle_async(:loading, {:ok, :error}, socket) do
+    {:noreply, redirect(socket, to: ~p"/tutor/my-courses")}
+  end
+
+  def handle_async(:loading, {:exit, reason}, socket) do
+    next_socket =
+      socket
+      |> assign(:loading, AsyncResult.failed(socket.assigns.loading, reason))
+
+    {:noreply, next_socket}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <.async_result assign={@loading}>
+      <:loading><.loading_spinner /></:loading>
+      <:failed>Failed...</:failed>
+
+      <CommonComponents.navigate_breadcrumbs links={[
+        {~p"/controls", "home"},
+        {~p"/controls/courses/#{@course.id}", @course.name},
+        {~p"/controls/courses/#{@course.id}/lessons", "lessons"}
+      ]} />
+
+      <div class="flex justify-end">
+        <.button id="new-button" phx-click="prep-create" class="px-5 flex items-center">
+          Add lesson <.icon name="hero-plus-circle-solid ml-2" />
+        </.button>
+      </div>
+
+      <ul id="lessons" phx-update="stream" class="mt-10">
+        <li :for={{id, lesson} <- @streams.lessons} id={id} class="flex items-center gap-2">
+          <.link navigate={~p"/controls/courses/#{@course.id}/lessons/#{lesson.id}"}>
+            <strong class="text-lg underline lg:no-underline hover:underline">
+              <%= lesson.title %>
+            </strong>
+          </.link>
+
+          <span
+            id={"lessons-#{lesson.id}-edit"}
+            phx-click={
+              JS.push("prep-edit", value: %{id: lesson.id}, page_loading: true)
+              |> show_modal("form-modal")
+            }
+            class="ml-auto hover:cursor-pointer"
+          >
+            <.icon name="hero-pencil-solid text-secondary hover:text-blue-300" />
+          </span>
+
+          <span id={"lessons-#{lesson.id}-delete"} phx-click="delete" phx-value-id={lesson.id}>
+            <.icon name="hero-trash-solid text-secondary hover:text-red-500 hover:cursor-pointer" />
+          </span>
+        </li>
+      </ul>
+
+      <.modal :if={@show_form_modal} id="form-modal" on_cancel={JS.push("clear")} show>
+        <.form for={@form} phx-change="validate" phx-submit={if @lesson, do: "edit", else: "create"}>
+          <.input type="text" label="Title" field={@form[:title]} />
+          <.input type="textarea" label="Description" field={@form[:description]} />
+
+          <div class="mt-5 flex justify-end">
+            <.button class="px-8">Create</.button>
+          </div>
+        </.form>
+      </.modal>
+    </.async_result>
+    """
+  end
+
+  def handle_event("clear", _, socket) do
+    form = Learning.Course.Content.create_lesson_changeset(socket.assigns.course) |> to_form()
+
+    {:noreply,
+     socket |> assign(:lesson, nil) |> assign(:form, form) |> assign(:show_form_modal, false)}
+  end
+
+  def handle_event("prep-create", _, socket) do
+    {:noreply, assign(socket, :show_form_modal, true)}
+  end
+
+  def handle_event("prep-edit", %{"id" => id}, socket) do
+    lesson = Learning.Course.Content.get_lesson!(socket.assigns.course.id, id)
+    cs = Learning.Course.Content.edit_lesson_changeset(lesson, %{})
+    form = to_form(cs)
+
+    {:noreply,
+     socket |> assign(:lesson, lesson) |> assign(form: form) |> assign(:show_form_modal, true)}
+  end
+
+  def handle_event("edit", %{"lesson" => params}, socket) do
+    case Learning.Course.Content.update_lesson(socket.assigns.lesson, params) do
+      {:ok, lesson} ->
+        next_form =
+          Learning.Course.Content.create_lesson_changeset(socket.assigns.course) |> to_form()
+
+        {:noreply,
+         socket
+         |> stream_insert(:lessons, lesson)
+         |> assign(:lesson, nil)
+         |> assign(:form, next_form)
+         |> assign(:show_form_modal, false)}
+
+      {:error, cs} ->
+        {:noreply, assign(socket, form: to_form(cs))}
+    end
+  end
+
+  def handle_event("create", %{"lesson" => params}, socket) do
+    case Learning.Course.Content.create_lesson(socket.assigns.course, params) do
+      {:ok, lesson} ->
+        next_form =
+          Learning.Course.Content.create_lesson_changeset(socket.assigns.course) |> to_form()
+
+        {:noreply,
+         stream_insert(socket, :lessons, lesson)
+         |> assign(:show_form_modal, false)
+         |> assign(:form, next_form)}
+
+      {:error, cs} ->
+        {:noreply, assign(socket, form: to_form(cs))}
+    end
+  end
+
+  def handle_event("validate", %{"lesson" => params}, socket) do
+    cs = Learning.Course.Content.validate_lesson(socket.assigns.course, params)
+    form = to_form(cs)
+    {:noreply, assign(socket, form: form)}
+  end
+
+  # TODO: Replace this insecure code, attacker can use
+  # the id parameter to delete any lesson on the system. 
+  def handle_event("delete", %{"id" => raw_id}, socket) do
+    raw_id
+    |> String.to_integer()
+    |> Learning.Course.Content.remove_lesson()
+
+    {:noreply, socket |> stream_delete_by_dom_id(:lessons, "lessons-#{raw_id}")}
+  end
+
+  def handle_info({:course, _, :lesson_added, lesson}, socket) do
+    {:noreply, stream_insert(socket, :lessons, lesson)}
+  end
+
+  def handle_info({:course, _, :lesson_deleted, lesson}, socket) do
+    {:noreply, stream_delete(socket, :lessons, lesson)}
+  end
+
+  def handle_info({:course, _, :lesson_updated, lesson}, socket) do
+    {:noreply, stream_insert(socket, :lessons, lesson)}
+  end
+
+  def handle_info({:course, _, :course_updated, course}, socket) do
+    {:noreply, assign(socket, :course, course)}
+  end
+
+  def handle_info({:course, _, _, _}, socket), do: {:noreply, socket}
+end
---
+++ test/lukas_web/live/lecturers/courses/lecturer_course_lessons_live_test.exs
@@ -0,0 +1,251 @@
+defmodule LukasWeb.Lecturer.CourseLessonsLiveTest do
+  use LukasWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+  import Lukas.LearningFixtures
+
+  alias Lukas.Learning
+
+  setup :register_and_log_in_lecturer
+
+  setup %{user: user} do
+    course = course_fixture()
+    {:ok, _} = Learning.Course.Staff.add_lecturer_to_course(course, user)
+    %{course: course}
+  end
+
+  describe "index" do
+    test "should redirect if the id is not valid.", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/invalid/lessons")
+      assert_redirect(lv, ~p"/tutor/my-courses")
+    end
+
+    test "should redirect if the course does not exist", %{conn: conn} do
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/300/lessons")
+      assert_redirect(lv, ~p"/tutor/my-courses")
+    end
+
+    test "should redirect if the user has no access to the course.", %{conn: conn} do
+      course = course_fixture()
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+      assert_redirect(lv, ~p"/tutor/my-courses")
+    end
+
+    test "should render the course name and lessons.", %{conn: conn, course: course} do
+      l1 = lesson_fixture(course)
+      l2 = lesson_fixture(course)
+      l3 = lesson_fixture(course)
+
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      html = render_async(lv)
+
+      assert html =~ course.name
+      assert html =~ l1.title
+      assert html =~ l2.title
+      assert html =~ l3.title
+    end
+
+    test "should react to lessons being added.", %{conn: conn, course: course} do
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      l1 = lesson_fixture(course)
+      l2 = lesson_fixture(course)
+      l3 = lesson_fixture(course)
+
+      html = render_async(lv)
+
+      assert html =~ course.name
+      assert html =~ l1.title
+      assert html =~ l2.title
+      assert html =~ l3.title
+    end
+
+    test "should react to lessons being removed.", %{conn: conn, course: course} do
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      l1 = lesson_fixture(course)
+      l2 = lesson_fixture(course)
+      l3 = lesson_fixture(course)
+
+      html = render_async(lv)
+
+      assert html =~ course.name
+      assert html =~ l1.title
+      assert html =~ l2.title
+      assert html =~ l3.title
+
+      {:ok, _} = Learning.Course.Content.remove_lesson(l1)
+
+      post_html = render(lv)
+
+      refute post_html =~ l1.title
+      assert post_html =~ l2.title
+      assert post_html =~ l3.title
+    end
+
+    test "should react to lessons being edited.", %{conn: conn, course: course} do
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      l1 = lesson_fixture(course)
+      l2 = lesson_fixture(course)
+      l3 = lesson_fixture(course)
+
+      html = render_async(lv)
+
+      assert html =~ course.name
+      assert html =~ l1.title
+      assert html =~ l2.title
+      assert html =~ l3.title
+
+      {:ok, next_l1} = Learning.Course.Content.update_lesson(l1, %{"title" => "New Title"})
+
+      post_html = render(lv)
+
+      refute post_html =~ l1.title
+
+      assert post_html =~ next_l1.title
+      assert post_html =~ l2.title
+      assert post_html =~ l3.title
+    end
+
+    test "should react to course being updated.", %{conn: conn, course: course} do
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      assert render_async(lv) =~ course.name
+
+      {:ok, updated} = Learning.update_course(course, %{"name" => "FOO IS GREAT"})
+
+      post_update_html = render_async(lv)
+
+      refute post_update_html =~ course.name
+      assert post_update_html =~ updated.name
+    end
+  end
+
+  describe "new" do
+    test "should render errors on change.", %{conn: conn, course: course} do
+      {:ok, lv, _} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      render_async(lv)
+
+      lv |> element("#new-button") |> render_click()
+
+      lv
+      |> form("form", %{"lesson" => %{"title" => "", "description" => "foo is great bar is none"}})
+      |> render_change()
+
+      assert render(lv) =~ "can&#39;t be blank"
+    end
+
+    test "should render errors on submit.", %{conn: conn, course: course} do
+      {:ok, lv, _} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      render_async(lv)
+
+      lv |> element("#new-button") |> render_click()
+
+      lv
+      |> form("form", %{"lesson" => %{"title" => "", "description" => "foo is great bar is none"}})
+      |> render_submit()
+
+      assert render(lv) =~ "can&#39;t be blank"
+    end
+
+    test "should create and render the new lesson on screen.", %{conn: conn, course: course} do
+      {:ok, lv, _} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      render_async(lv)
+
+      lv |> element("#new-button") |> render_click()
+
+      lv
+      |> form("form", %{
+        "lesson" => %{"title" => "Cool title", "description" => "foo is great bar is none"}
+      })
+      |> render_submit()
+
+      assert render(lv) =~ "Cool title"
+    end
+  end
+
+  describe "edit" do
+    test "should render errors on change.", %{conn: conn, course: course} do
+      l1 = lesson_fixture(course)
+
+      {:ok, lv, _} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      render_async(lv)
+
+      lv |> element("#lessons-#{l1.id}-edit") |> render_click()
+
+      lv
+      |> form("form", %{"lesson" => %{"title" => "", "description" => "foo is great bar is none"}})
+      |> render_change()
+
+      assert render(lv) =~ "can&#39;t be blank"
+    end
+
+    test "should render errors on submit.", %{conn: conn, course: course} do
+      l1 = lesson_fixture(course)
+
+      {:ok, lv, _} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      render_async(lv)
+
+      lv |> element("#lessons-#{l1.id}-edit") |> render_click()
+
+      lv
+      |> form("form", %{"lesson" => %{"title" => "", "description" => "foo is great bar is none"}})
+      |> render_submit()
+
+      assert render(lv) =~ "can&#39;t be blank"
+    end
+
+    test "should edit the lesson.", %{conn: conn, course: course} do
+      l1 = lesson_fixture(course)
+      l2 = lesson_fixture(course)
+
+      {:ok, lv, _} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+
+      render_async(lv)
+
+      lv |> element("#lessons-#{l1.id}-edit") |> render_click()
+
+      lv
+      |> form("form", %{
+        "lesson" => %{
+          "title" => "Next title for edit",
+          "description" => "foo is great bar is none"
+        }
+      })
+      |> render_submit()
+
+      html = render(lv)
+
+      assert html =~ "Next title for edit"
+      assert html =~ l2.title
+
+      refute html =~ l1.title
+    end
+  end
+
+  describe "delete" do
+    test "should render a button for deleting the course lesson.", %{conn: conn, course: course} do
+      l1 = lesson_fixture(course)
+      l2 = lesson_fixture(course)
+      l3 = lesson_fixture(course)
+
+      {:ok, lv, _html} = live(conn, ~p"/tutor/my-courses/#{course.id}/lessons")
+      render_async(lv)
+
+      lv |> element("#lessons-#{l1.id}-delete") |> render_click()
+
+      html = render(lv)
+
+      assert html =~ l2.title
+      assert html =~ l3.title
+      refute html =~ l1.title
+    end
+  end
+end
