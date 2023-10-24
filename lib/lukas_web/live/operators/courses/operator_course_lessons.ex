2623f1c582ef2b8b1e601006bb4208cd5a5f7491
mse99 <mohamedsalemedrah@gmail.com>
2023-10-24 17:21:08 +0200

    Style operator course lessons page

--- lib/lukas_web/live/operators/courses/operator_course_lessons.ex
+++ lib/lukas_web/live/operators/courses/operator_course_lessons.ex
@@ -3,8 +3,8 @@ defmodule LukasWeb.Operator.CourseLessonsLive do
 
   alias Lukas.Learning
 
+  alias LukasWeb.CommonComponents
   alias Phoenix.LiveView.AsyncResult
-  alias Lukas.Learning
 
   def mount(params, _session, socket) do
     {:ok,
@@ -58,28 +58,40 @@ defmodule LukasWeb.Operator.CourseLessonsLive do
       <:loading><.loading_spinner /></:loading>
       <:failed>Failed...</:failed>
 
-      <h1><%= @course.name %></h1>
+      <CommonComponents.navigate_breadcrumbs links={[
+        {~p"/controls", "home"},
+        {~p"/controls/courses/#{@course.id}", @course.name},
+        {~p"/controls/courses/#{@course.id}/lessons", "lessons"}
+      ]} />
 
-      <.button id="new-button" phx-click="prep-create">
-        Create
-      </.button>
+      <div class="flex justify-end">
+        <.button id="new-button" phx-click="prep-create" class="px-5 flex items-center">
+          Add lesson <.icon name="hero-plus-circle-solid ml-2" />
+        </.button>
+      </div>
 
-      <ul id="lessons" phx-update="stream">
-        <li :for={{id, lesson} <- @streams.lessons} id={id}>
-          <%= lesson.title %>
-          <.button
+      <ul id="lessons" phx-update="stream" class="mt-10">
+        <li :for={{id, lesson} <- @streams.lessons} id={id} class="flex items-center gap-2">
+          <.link navigate={~p"/controls/courses/#{@course.id}/lessons/#{lesson.id}"}>
+            <strong class="text-lg underline lg:no-underline hover:underline">
+              <%= lesson.title %>
+            </strong>
+          </.link>
+
+          <span
             id={"lessons-#{lesson.id}-edit"}
             phx-click={
               JS.push("prep-edit", value: %{id: lesson.id}, page_loading: true)
               |> show_modal("form-modal")
             }
+            class="ml-auto hover:cursor-pointer"
           >
-            Edit
-          </.button>
+            <.icon name="hero-pencil-solid text-secondary hover:text-blue-300" />
+          </span>
 
-          <.button id={"lessons-#{lesson.id}-delete"} phx-click="delete" phx-value-id={lesson.id}>
-            Delete
-          </.button>
+          <span id={"lessons-#{lesson.id}-delete"} phx-click="delete" phx-value-id={lesson.id}>
+            <.icon name="hero-trash-solid text-secondary hover:text-red-500 hover:cursor-pointer" />
+          </span>
         </li>
       </ul>
 
