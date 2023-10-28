6507c9afc85afe7dd9fa18e2b0167fe61204d937
mse99 <mohamedsalemedrah@gmail.com>
2023-10-29 01:32:10 +0200

    Translate operator invites page

--- lib/lukas_web/live/operators/invites_live.ex
+++ lib/lukas_web/live/operators/invites_live.ex
@@ -35,8 +35,8 @@ defmodule LukasWeb.Operator.InvitesLive do
   def render(assigns) do
     ~H"""
     <CommonComponents.navigate_breadcrumbs links={[
-      {~p"/controls", "home"},
-      {~p"/controls/invites", "invites"}
+      {~p"/controls", gettext("home")},
+      {~p"/controls/invites", gettext("invites")}
     ]} />
 
     <.async_result assign={@loading_result}>
@@ -48,17 +48,21 @@ defmodule LukasWeb.Operator.InvitesLive do
 
       <div class="flex justify-center md:justify-end gap-2 mb-10">
         <.button id="generate-lecturer-invite-button" phx-click="generate-lecturer-invite">
-          Create lecturer invite
+          <%= gettext("Create lecturer invite") %>
         </.button>
 
         <.button id="generate-operator-invite-button" phx-click="generate-operator-invite">
-          Create operator invite
+          <%= gettext("Create operator invite") %>
         </.button>
       </div>
 
       <div class="text-secondary font-bold gap-2 mb-5">
-        <span class="ms-4">Code</span>
-        <span>Kind</span>
+        <span class="me-4">
+          <%= gettext("Code") %>
+        </span>
+        <span>
+          <%= gettext("Kind") %>
+        </span>
       </div>
 
       <ul id="invites" phx-update="stream">
@@ -68,7 +72,7 @@ defmodule LukasWeb.Operator.InvitesLive do
           class="flex items-center gap-2 mb-5 border-b pb-2"
         >
           <span><%= inv.code %></span>
-          <span><%= inv.kind %></span>
+          <span><%= invite_kind_to_string(inv.kind) %></span>
 
           <div
             class="delete-invite-button ms-auto hover:cursor-pointer"
@@ -106,4 +110,7 @@ defmodule LukasWeb.Operator.InvitesLive do
   def handle_info({:invites, :invite_deleted, inv}, socket) do
     {:noreply, stream_delete(socket, :invites, inv)}
   end
+
+  def invite_kind_to_string(:lecturer), do: gettext("lecturer")
+  def invite_kind_to_string(:operator), do: gettext("operator")
 end
