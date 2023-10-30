4f1bc23afc50effb625731c52180d1346c0515d3
mse99 <mohamedsalemedrah@gmail.com>
2023-10-30 20:59:19 +0200

    Translate login page

--- lib/lukas_web/live/shared/user_login_live.ex
+++ lib/lukas_web/live/shared/user_login_live.ex
@@ -11,29 +11,30 @@ defmodule LukasWeb.UserLoginLive do
     ~H"""
     <div class="mx-auto max-w-sm">
       <.header>
-        Login
+        <%= gettext("Login") %>
+
         <:subtitle>
-          Please enter your phone number and password
+          <%= gettext("Please enter your phone number and password") %>
         </:subtitle>
       </.header>
 
       <.form for={@form} class="mt-5" id="login_form" action={~p"/users/log_in"} phx-update="ignore">
         <div class="mb-5">
-          <.input field={@form[:phone_number]} type="tel" label="Phone number" required />
+          <.input field={@form[:phone_number]} type="tel" label={gettext("Phone number")} required />
         </div>
 
         <div class="mb-5">
-          <.input field={@form[:password]} type="password" label="Password" required />
+          <.input field={@form[:password]} type="password" label={gettext("Password")} required />
         </div>
 
-        <.button phx-disable-with="Signing in..." class="w-full">
-          Sign in <span aria-hidden="true">→</span>
+        <.button phx-disable-with={gettext("Signing in...")} class="w-full">
+          <%= gettext("Sign in") %>
         </.button>
       </.form>
 
       <div class="mt-5 font-bold underline">
         <.link href={~p"/users/register"}>
-          Register a new account
+          <%= gettext("Register a new account") %>
         </.link>
       </div>
     </div>
