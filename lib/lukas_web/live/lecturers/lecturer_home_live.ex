1a6618ff84022e0741f1cbf10d802f3ecea3a2db
mse99 <mohamedsalemedrah@gmail.com>
2023-11-03 18:34:33 +0200

    Translate lecturer homepage

--- lib/lukas_web/live/lecturers/lecturer_home_live.ex
+++ lib/lukas_web/live/lecturers/lecturer_home_live.ex
@@ -8,30 +8,31 @@ defmodule LukasWeb.Lecturer.HomeLive do
   def render(assigns) do
     ~H"""
     <.greeting>
-      Hello <%= @current_user.name %>
+      <%= gettext("Hello") %>
+      <%= @current_user.name %>
     </.greeting>
 
     <ul class="flex flex-col gap-2 mt-5">
       <li>
         <.link class="bg-white shadow p-5 block max-w-xs" navigate={~p"/tutor/my-courses"}>
-          <.icon name="hero-academic-cap-solid" class="me-5" />My courses
+          <.icon name="hero-academic-cap-solid" class="me-5" /><%= gettext("My courses") %>
         </.link>
       </li>
       <li>
         <.link class="bg-white shadow p-5 block max-w-xs" href={~p"/users/settings"}>
-          <.icon name="hero-cog-6-tooth-solid" class="me-5" />settings
+          <.icon name="hero-cog-6-tooth-solid" class="me-5" /><%= gettext("settings") %>
         </.link>
       </li>
     </ul>
 
     <.link href={~p"/users/log_out"} method="DELETE" class="flex justify-end mt-5">
       <.danger_button>
-        Logout
+        <%= gettext("Logout") %>
       </.danger_button>
     </.link>
 
     <.link href={~p"/locale"} method="patch">
-      Switch locale :D
+      <.icon name="hero-language" />
     </.link>
     """
   end
