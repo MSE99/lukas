ca6713521d17ab3ac10e0293af370e74b3dc805c
mse99 <mohamedsalemedrah@gmail.com>
2023-11-18 19:51:51 +0200

    Redesign student's homepage

--- lib/lukas_web/live/students/students_home_live.ex
+++ lib/lukas_web/live/students/students_home_live.ex
@@ -9,28 +9,64 @@ defmodule LukasWeb.Students.HomeLive do
       Hello <%= @current_user.name %>
     </h1>
 
-    <ul class="flex flex-col gap-2">
-      <li>
-        <.link class="bg-white shadow p-5 block max-w-xs" navigate={~p"/home/courses"}>
-          <.icon name="hero-academic-cap-solid" class="me-5" /><%= gettext("My courses") %>
+    <ul class="flex flex-col w-full gap-2 mt-5 justify-center md:justify-start">
+      <li class="max-w-xs w-full h-full">
+        <.link
+          navigate={~p"/home/courses"}
+          class="bg-white shadow rounded-lg h-28 hover:outline outline-green-300 transition-all transition-100 p-5 flex flex-col max-w-xs"
+        >
+          <div class="flex items-center">
+            <.icon name="hero-academic-cap-solid me-3" /> <%= gettext("My courses") %>
+          </div>
+
+          <p class="mt-3 text-sm text-secondary">
+            <%= gettext("View all courses on your platform or add new ones.") %>
+          </p>
         </.link>
       </li>
 
-      <li>
-        <.link class="bg-white shadow p-5 block max-w-xs" navigate={~p"/home/courses/available"}>
-          <.icon name="hero-academic-cap-solid" class="me-5" /><%= gettext("Other courses") %>
+      <li class="max-w-xs w-full h-full">
+        <.link
+          navigate={~p"/users/settings"}
+          class="bg-white shadow rounded-lg h-28 hover:outline outline-green-300 transition-all transition-100 p-5 flex flex-col max-w-xs"
+        >
+          <div class="flex items-center">
+            <.icon name="hero-cog-solid me-3" /> <%= gettext("Settings") %>
+          </div>
+
+          <p class="mt-3 text-sm text-secondary">
+            <%= gettext("Update your accounts settings.") %>
+          </p>
         </.link>
       </li>
 
-      <li>
-        <.link class="bg-white shadow p-5 block max-w-xs" navigate={~p"/home/wallet"}>
-          <.icon name="hero-envelope-solid" class="me-5" /><%= gettext("My wallet") %>
+      <li class="max-w-xs w-full h-full">
+        <.link
+          navigate={~p"/home/courses/available"}
+          class="bg-white shadow rounded-lg h-28 hover:outline outline-green-300 transition-all transition-100 p-5 flex flex-col max-w-xs"
+        >
+          <div class="flex items-center">
+            <.icon name="hero-academic-cap-solid" class="me-3" /> <%= gettext("Other courses") %>
+          </div>
+
+          <p class="mt-3 text-sm text-secondary">
+            <%= gettext("Update your accounts settings.") %>
+          </p>
         </.link>
       </li>
 
-      <li>
-        <.link class="bg-white shadow p-5 block max-w-xs" href={~p"/users/settings"}>
-          <.icon name="hero-cog-6-tooth-solid" class="me-5" /><%= gettext("settings") %>
+      <li class="max-w-xs w-full h-full">
+        <.link
+          navigate={~p"/home/wallet"}
+          class="bg-white shadow rounded-lg h-28 hover:outline outline-green-300 transition-all transition-100 p-5 flex flex-col max-w-xs"
+        >
+          <div class="flex items-center">
+            <.icon name="hero-envelope-solid" class="me-3" /> <%= gettext("My wallet") %>
+          </div>
+
+          <p class="mt-3 text-sm text-secondary">
+            <%= gettext("View your funds and transactions.") %>
+          </p>
         </.link>
       </li>
     </ul>
